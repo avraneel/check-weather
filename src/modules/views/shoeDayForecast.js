fb4ffc2418a94dcf886359277309ceca482e96c9
Avraneel Pal <avraneelpal22@gmail.com>
2026-02-10 21:35:09 +0530

    Add views for this week

---
+++ src/modules/views/shoeDayForecast.js
@@ -0,0 +1,16 @@
+function showDayForecast(dayWeather) {
+  const forecastContainer = document.createElement("div");
+
+  const date = document.createElement("div");
+  const icon = document.createElement("img");
+  const temp = document.createElement("div");
+
+  date.textContent = dayWeather.datetime;
+  date;
+}
+
+/**
+ * TODO:
+ * - Map icon-name to icon
+ * - convert datetime to proper format
+ */
---
+++ src/modules/views/showThisWeek.js
@@ -0,0 +1,13 @@
+function showThisWeek(weekData) {
+  const weekWeather = document.createElement(".week-weather");
+
+  const h3 = document.createElement("h3");
+}
+
+function showDayForecast(dayWeather) {
+  const forecastContainer = document.createElement("div");
+
+  const date = document.createElement("div");
+  const icon = document.createElement("img");
+  const temp = document.createElement("div");
+}
