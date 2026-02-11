a17130803f469dbeb86ebd1a6544b6fe8a2fc735
Avraneel Pal <avraneelpal22@gmail.com>
2026-02-11 06:45:58 +0530

    Add astronomy icons

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { NextDay } from "./modules/NextData.js";
 
 async function getCurrentData(location) {
   const unit = "metric";
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bangalore?unitGroup=us&key=3SBV58YFMG5PWJYHX7M2NQ396&contentType=json`;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bangalore?unitGroup=us&elements=add%3Aaqielement%2Cadd%3Aaqieur%2Cadd%3Aaqius%2Cadd%3Aco%2Cadd%3Amoonrise%2Cadd%3Amoonset%2Cadd%3Ano2%2Cadd%3Ao3%2Cadd%3Apm1%2Cadd%3Apm10%2Cadd%3Apm2p5%2Cadd%3Aso2%2Cremove%3AdatetimeEpoch%2Cremove%3Asolarenergy%2Cremove%3Awindgust&key=3SBV58YFMG5PWJYHX7M2NQ396&contentType=json`;
 
   const data = await fetch(url).then((response) => response.json());
   const curr = data.currentConditions;
