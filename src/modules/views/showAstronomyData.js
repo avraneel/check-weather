817b8cca1205c90d6548d8e8e4d46c05fe982814
Avraneel Pal <avraneelpal22@gmail.com>
2026-02-11 23:38:26 +0530

    Begin Render Astronomy Data

---
+++ src/modules/views/showAstronomyData.js
@@ -0,0 +1,8 @@
+export function showAstronomyData(astronomyData) {
+  const astronomyContainer = document.createElement("div");
+
+  const h3 = document.createElement("h3");
+  h3.textContent = "Astronomy";
+
+  const astronomyBox = document.createElement("div");
+}
